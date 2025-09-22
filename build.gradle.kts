11218d6e38164c8a543652047f050bb08f9c23bc
chinychiu <chinychiu@gmail.com>
2025-09-22 14:10:13 +0800

    chore: 修改兼容清单

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.etrx"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
@@ -46,8 +46,7 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("232")
-        untilBuild.set("252.*")
+        sinceBuild.set("231")
     }
 
     // Local compatibility verification using the IntelliJ Plugin Verifier
